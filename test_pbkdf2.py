515f3592bfed719a5f52358291050e49125a1379
Josh Friend <jfriend@michiganlabs.com>
2014-12-12 21:07:20 -0500

    py.test ALL the things!

--- test_pbkdf2.py
+++ test_pbkdf2.py
@@ -1,22 +1,26 @@
-import unittest
+#!/usr/bin/env python
+
+import pytest
 
 import flask
 from flask_pbkdf2 import Pbkdf2
 
-class Pbkdf2TestCase(unittest.TestCase):
-    def setUp(self):
-        app = flask.Flask(__name__)
-        app.config['ITERATIONS'] = 1000
-        self.pbkdf2 =Pbkdf2(app)
+@pytest.fixture
+def pbkdf2():
+    app = flask.Flask(__name__)
+    app.config['ITERATIONS'] = 1000
+    pbkdf2 = Pbkdf2(app)
+    return pbkdf2
 
-    def test_check_password(self):
+
+class TestPbkdf2:
+    def test_check_password(self, pbkdf2):
         encoded = 'pbkdf2_sha256$1000$tablesalt$8cc051c0f3c5f2e20cdd8fd85d1eb02a911381ff0fa217c0'
-        self.assertTrue(self.pbkdf2.check_password('test',encoded))
-        with self.assertRaises(AssertionError):
-            self.pbkdf2.check_password('test','sha1$1000$tablesalt$8cc051c0f3c5f2e20cdd8fd85d1eb02a911381ff0fa217c0')
+        assert pbkdf2.check_password('test', encoded) is True
+        with pytest.raises(AssertionError):
+            pbkdf2.check_password('test', 'sha1$1000$tablesalt$8cc051c0f3c5f2e20cdd8fd85d1eb02a911381ff0fa217c0')
 
-    def test_make_password(self):
-        self.assertEqual('pbkdf2_sha256$1000$tablesalt$8cc051c0f3c5f2e20cdd8fd85d1eb02a911381ff0fa217c0',self.pbkdf2.make_password('test',salt='tablesalt'))
+    def test_make_password(self, pbkdf2):
+        encoded = pbkdf2.make_password('test', salt='tablesalt')
+        assert encoded == 'pbkdf2_sha256$1000$tablesalt$8cc051c0f3c5f2e20cdd8fd85d1eb02a911381ff0fa217c0'
 
-if __name__ == '__main__':
-    unittest.main()
